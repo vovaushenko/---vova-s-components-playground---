97333f7164b1e2826759149a3173972f675d10cf
vovaushenko <vovaushenko1989@gmail.com>
2021-10-03 21:05:34 +0300

    NextImage

---
+++ components/Image/NextImage.tsx
@@ -0,0 +1,57 @@
+import Image from 'next/image';
+import styled from 'styled-components';
+
+interface Props {
+  variant: 'full-viewport-bg' | 'card-image';
+}
+
+const NextImage: React.FunctionComponent<Props> = ({
+  variant,
+}): JSX.Element => {
+  return (
+    <>
+      {variant === 'full-viewport-bg' && (
+        <Image
+          src="/assets/toronto.jpg"
+          layout="fill"
+          objectFit="cover"
+          alt="Mesmerizing Toronto"
+        />
+      )}
+
+      {variant === 'card-image' && (
+        <Card>
+          <Image
+            src="/assets/toronto.jpg"
+            // just put the original width and height of the original image, in order to provide the right aspect ratio
+            // Next.js will automatically reduce the size if the rendered image needs to be smaller.
+            width={1920}
+            height={1280}
+            layout="responsive"
+            className="card-img-top"
+            alt="..."
+          />
+
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card`s content.
+            </p>
+          </div>
+        </Card>
+      )}
+    </>
+  );
+};
+
+const Card = styled.article`
+  width: 20rem;
+  padding: ${({ theme }) => theme.spacing.sm};
+  background: ${({ theme }) => theme.colors.gray.card};
+  .card-body {
+    margin-top: ${({ theme }) => theme.spacing.sm};
+  }
+`;
+
+export default NextImage;
--- sreens/Playgrounds/Playgrounds.tsx
+++ sreens/Playgrounds/Playgrounds.tsx
@@ -1,9 +1,5 @@
-import React, { useCallback, useState } from 'react';
-import GenericButton from '../../components/Buttons/GenericButton/GenericButton';
-import ContextMenu from '../../components/UI/ContextMenu/ContextMenu';
-import WithSparkles from '../../components/UI/Sparkles/WithSparkles';
-import { useGetMouseCoordinates } from '../../hooks/useGetMouseCoordinates';
-import { debounce } from '../../utils/index';
+import React from 'react';
+import NextImage from '../../components/Image/NextImage';
 import * as Styled from './Playgrounds.styles';
 
 /**
@@ -13,123 +9,9 @@ import * as Styled from './Playgrounds.styles';
  *@returns {JSX.Element} - Rendered Playgrounds component
  */
 const Playgrounds = (): JSX.Element => {
-  const [isStateMenuOpen, setIsStateMenuOpen] = useState<boolean>(false);
-  const toggleContextMenu = useCallback(() => {
-    setIsStateMenuOpen((prev) => !prev);
-  }, []);
-
-  const [mouseX, setMouseX] = React.useState(0);
-
-  const handleRightMouseClick = (
-    e: React.MouseEvent<HTMLDivElement, MouseEvent>
-  ) => {
-    e.preventDefault();
-    setIsStateMenuOpen(true);
-  };
-
-  const { x, y } = useGetMouseCoordinates();
-
-  const handleMouseMove = React.useCallback(
-    debounce((ev: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-      setMouseX(ev.clientX);
-    }, 250),
-    []
-  );
-
   return (
-    <Styled.Container
-      onContextMenu={handleRightMouseClick}
-      onMouseMove={handleMouseMove}
-    >
-      <WithSparkles color={'yellow'}>
-        <GenericButton onClick={toggleContextMenu}>CLICK</GenericButton>
-      </WithSparkles>
-      {isStateMenuOpen && (
-        <ContextMenu
-          locationCoordinates={{ x, y }}
-          contextMenuItems={[
-            { id: 1, text: 'View', action: null, withUnderline: true },
-            {
-              id: 2,
-              text: 'Sort by',
-              action: null,
-              withUnderline: true,
-              hoverMenuItems: [
-                { id: 1, text: 'View', action: null },
-                {
-                  id: 2,
-                  text: 'View',
-                  action: null,
-                  hoverMenuItems: [
-                    { id: 1, text: 'View', action: null },
-                    { id: 2, text: 'View', action: null },
-                  ],
-                },
-                {
-                  id: 3,
-                  text: 'View',
-                  action: null,
-                },
-              ],
-            },
-            {
-              id: 3,
-              text: 'Refresh',
-              action: () => alert('Refresh'),
-            },
-            { id: 4, text: 'View', action: null },
-            {
-              id: 5,
-              text: 'Items',
-              action: null,
-              withUnderline: true,
-              hoverMenuItems: [
-                { id: 1, text: 'View', action: null },
-                { id: 2, text: 'View', action: null },
-                {
-                  id: 3,
-                  text: 'View',
-                  action: null,
-                },
-                {
-                  id: 4,
-                  text: 'Refresh',
-                  action: () => alert('Refresh'),
-                },
-                {
-                  id: 5,
-                  text: 'Refresh',
-                  action: () => alert('Refresh'),
-                },
-                {
-                  id: 6,
-                  text: 'Refresh',
-                  action: () => alert('Refresh'),
-                },
-                {
-                  id: 7,
-                  text: 'Refresh',
-                  action: () => alert('Refresh'),
-                },
-                {
-                  id: 8,
-                  text: 'Refresh',
-                  action: () => alert('Refresh'),
-                },
-              ],
-            },
-
-            {
-              id: 5,
-              text: 'Refresh',
-              action: () => alert('Refresh'),
-            },
-          ]}
-        />
-      )}
-
-      <pre>{`X${x} Y${y}`}</pre>
-      <pre>{`X${mouseX}`}</pre>
+    <Styled.Container>
+      <NextImage variant="card-image" />
     </Styled.Container>
   );
 };
